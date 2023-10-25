45012c5cd5d348e3a16b94f85da8156ffb3ee025
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-10-25 17:20:22 -0400

    started...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,17 @@ public class Main {
 
     public static void main(String[] args ){
 
+        String name;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your name: ");
+        name = input.next();
+
+        if(name.equalsIgnoreCase("alice") || name.equalsIgnoreCase("bob")){
+            System.out.println("Hello, " + name + "!");
+        }
+        else{
+            System.out.println("You are neither Alice nor Bob.");
+        }
     }
 }
